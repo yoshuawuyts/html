92f936f0c6454819e72a0b4f7d11eb9ae00155e4
Yosh <github@yosh.is>
2023-04-06 14:29:42 +0200

    tweak things slightly

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -19,43 +19,50 @@ enum Opt {
 #[async_std::main]
 async fn main() -> Result<()> {
     match Opt::from_args() {
-        Opt::Generate => {
-            let idl_path = std::env::current_dir()?.join("resources/webidls");
-
-            // generate IDL files
-            let database = html_bindgen::parse(&idl_path)?;
-            let path = std::env::current_dir()?.join("resources/nodes");
-            fs::create_dir_all(&path)?;
-            for def in database {
-                let path = path.join(format!("{}.json", def.tag_name));
-
-                let s = serde_json::to_string_pretty(&def)?;
-                std::fs::write(path, s.to_string().as_bytes())?;
-            }
-
-            // generate bindings
-            let s = html_bindgen::generate_html(&idl_path)?;
-
-            // write
-            let path = std::env::current_dir()?.join("crates/html-sys/src/lib.rs");
-            std::fs::write(path, s.as_bytes())?;
-            Ok(())
-        }
-        Opt::Scrape => {
-            let path = std::env::current_dir()?.join("resources/html-standard/index.html");
-            let spec = fs::read_to_string(path)?;
-            html_bindgen::parse_spec(spec)?;
-            Ok(())
-        }
-        Opt::Fetch => {
-            let target_name = "resources/html-standard/index.html";
-            let body = surf::get("https://html.spec.whatwg.org")
-                .recv_string()
-                .await?;
-            let mut target = async_std::fs::File::create(target_name).await?;
-            target.write_all(body.as_bytes()).await?;
-            eprintln!("updated: `{target_name}`");
-            Ok(())
-        }
+        Opt::Generate => generate()?,
+        Opt::Scrape => scrape()?,
+        Opt::Fetch => fetch().await?,
     }
+    Ok(())
+}
+
+fn generate() -> Result<()> {
+    let idl_path = std::env::current_dir()?.join("resources/webidls");
+
+    // generate IDL files
+    let database = html_bindgen::parse(&idl_path)?;
+    let path = std::env::current_dir()?.join("resources/nodes");
+    fs::create_dir_all(&path)?;
+    for def in database {
+        let path = path.join(format!("{}.json", def.tag_name));
+
+        let s = serde_json::to_string_pretty(&def)?;
+        std::fs::write(path, s.to_string().as_bytes())?;
+    }
+
+    // generate bindings
+    let s = html_bindgen::generate_html(&idl_path)?;
+
+    // write
+    let path = std::env::current_dir()?.join("crates/html-sys/src/lib.rs");
+    std::fs::write(path, s.as_bytes())?;
+    Ok(())
+}
+
+async fn fetch() -> Result<()> {
+    let target_name = "resources/html-standard/index.html";
+    let url = "https://html.spec.whatwg.org";
+    eprintln!("fetching: {url}");
+    let body = surf::get(url).recv_string().await?;
+    let mut target = async_std::fs::File::create(target_name).await?;
+    target.write_all(body.as_bytes()).await?;
+    eprintln!("updated: {target_name}");
+    Ok(())
+}
+
+fn scrape() -> Result<()> {
+    let path = std::env::current_dir()?.join("resources/html-standard/index.html");
+    let spec = fs::read_to_string(path)?;
+    html_bindgen::parse_spec(spec)?;
+    Ok(())
 }
