5c8a8f6ba3b2cecd90910c427b761ffa8f8d7cc8
Yosh <github@yosh.is>
2023-04-07 13:13:39 +0200

    comment out tests

--- crates/html-sys/tests/test.rs
+++ crates/html-sys/tests/test.rs
@@ -1,9 +1,9 @@
-use html_sys::*;
+// use html_sys::*;
 
-#[test]
-fn smoke() {
-    let button = HTMLButtonElement::default();
-    let mut text = Text::default();
-    text.set_data("hello".to_owned());
-    assert_eq!(button.to_string(), "<button></button>");
-}
+// #[test]
+// fn smoke() {
+//     let button = HTMLButtonElement::default();
+//     let mut text = Text::default();
+//     text.set_data("hello".to_owned());
+//     assert_eq!(button.to_string(), "<button></button>");
+// }
