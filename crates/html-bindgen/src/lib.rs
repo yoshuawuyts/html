93aa9a9f38795508317f499c4288f56a210278a2
Yosh <github@yosh.is>
2023-04-07 19:49:25 +0200

    generate -> generate_sys

--- crates/html-bindgen/src/lib.rs
+++ crates/html-bindgen/src/lib.rs
@@ -1,8 +1,8 @@
-mod generate;
+mod generate_sys;
 mod parse;
 mod scrape;
 mod types;
 
-pub use generate::generate;
+pub use generate_sys::generate;
 pub use parse::{parse, Attribute, ParsedNode};
 pub use scrape::{scrape_spec, ScrapedNode};
