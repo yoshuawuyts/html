5405466f8f1e5ddc02ec1e3fdc70f5a7e9363355
Yosh <github@yosh.is>
2023-04-08 23:37:51 +0200

    strip out <search>

--- crates/html/src/generated/text/search.rs
+++
@@ -1,8 +0,0 @@
-/// The HTML `<search>` element
-///
-/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/search)
-#[doc(alias = "search")]
-#[non_exhaustive]
-pub struct Search {
-    _sys: html_sys::text::Search,
-}
