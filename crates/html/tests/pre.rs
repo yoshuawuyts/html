767ca3fc24daedb5193769adb316912b70a0adb9
Yosh <github@yosh.is>
2023-08-06 23:18:53 +0200

    Fix indentation of <pre> tags

---
+++ crates/html/tests/pre.rs
@@ -0,0 +1,38 @@
+#![recursion_limit = "512"]
+
+use html::root::Body;
+use html::text_content::PreformattedText;
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+
+#[test]
+fn empty_pre() {
+    assert_eq!(
+        PreformattedText::builder().build().to_string(),
+        "<pre></pre>".to_string()
+    )
+}
+
+#[test]
+fn test_pre() {
+    assert_eq!(
+        PreformattedText::builder().text("test").build().to_string(),
+        "<pre>test</pre>".to_string()
+    )
+}
+
+#[test]
+fn regular_pre() {
+    let el = Body::builder()
+        .preformatted_text(|pre| pre.text("hello"))
+        .build();
+
+    assert_eq!(
+        el.to_string(),
+        indoc!(
+            r#"<body>
+                   <pre>hello</pre>
+               </body>"#
+        )
+    )
+}
